c43a0bfa51ded602e9d7cb2888304f81c2d911b2
Oliver Dahl <oliverpdahl@gmail.com>
2020-04-10 15:15:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function updateDriverWithKeyAndValue(driver, key, value) {
 }
 
 function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
-  driver['key'] = value;
+  driver[key] = value;
   return driver
 }
